3d9f79cb7417647ce18db368071bc5b4bda7c7c7
llowenberg <lancelowenberg@gmail.com>
2017-09-07 15:08:51 -0700

    assignment 5

---
+++ scripts/go.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rostopic pub -1 \cmd_drive kingfisher_msgs/Drive  '{left: -0.4, right: 0.4}'
+rostopic pub -1 \cmd_drive kingfisher_msgs/Drive  '{left: -0.4, right: 0.4}'
+rostopic pub -1 \cmd_drive kingfisher_msgs/Drive  '{left: 1.0, right: 1.0}'
+rostopic pub -1 \cmd_drive kingfisher_msgs/Drive  '{left: 1.0, right: 1.0}'
+rostopic pub -1 \cmd_drive kingfisher_msgs/Drive  '{left: 1.0, right: 1.0}'
+rostopic pub -1 \cmd_drive kingfisher_msgs/Drive  '{left: 1.0, right: 1.0}'
+rostopic pub -1 \cmd_drive kingfisher_msgs/Drive  '{left: 0.4, right: -0.4}'
+rostopic pub -1 \cmd_drive kingfisher_msgs/Drive  '{left: 1.0, right: 1.0}'
+rostopic pub -1 \cmd_drive kingfisher_msgs/Drive  '{left: 1.0, right: 1.0}'
+rostopic pub -1 \cmd_drive kingfisher_msgs/Drive  '{left: 1.0, right: 1.0}'
+rostopic pub -1 \cmd_drive kingfisher_msgs/Drive  '{left: 1.0, right: 1.0}'
+rostopic pub -1 \cmd_drive kingfisher_msgs/Drive  '{left: 0.4, right: -0.4}'
+rostopic pub -r 1 \cmd_drive kingfisher_msgs/Drive  '{left: 1.0, right: 1.0}'
